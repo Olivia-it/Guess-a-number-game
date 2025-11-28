5ea6837412a760664045e1df836a81a2673c5111
BenFleck25 <ben.fleck2508@outlook.com>
2025-11-28 14:13:00 +0000

    update

--- script.js
+++ script.js
@@ -9,17 +9,16 @@
 
 const prompt=require("prompt-sync")({sigint:true})
 let userInp = prompt("Enter a number between 1 and 10: ")
-
+let parsedInp = parseInt(userInp)
 // function guessNumber(num){
-if (userInp === 7){
+if (parsedInp === 7){
         console.log('Correct Number!')
-    } else if (userInp > 10){
+    } else if (parsedInp > 10){
         console.log('Number must be between 1 and 10')
-    } else if (typeof userInp !== 'number'){
-        console.log('Input must be a number!')
-    } else if (userInp < 0){
+    } else if (typeof parsedInp == 'string'){
+        console.log('string detected!')
+    } else if (parsedInp < 0){
         console.log('Number must be positive')
     } else {
         console.log('Try again!')
     }
-    console.log(userInp);
