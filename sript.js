b8fa3847450ea4cbddbd274fff6e519d4e824a61
BenFleck25 <ben.fleck2508@outlook.com>
2025-11-28 10:55:22 +0000

    started making event listener for form

--- sript.js
+++ sript.js
@@ -1,12 +1,22 @@
-const subButton = document.querySelector('')
+const numForm = document.querySelector('#numForm')
+const numResult = document.querySelector('#result')
 
+numForm.addEventListener('submit', guessNumber(e){
+    e.preventDefault();
+    const
+
+})
 
 function guessNumber(num){
     if (num === 7){
         return 'Correct Number!'
     } else if (num > 10){
         return 'Number must be between 1 and 10'
+    } else if (typeof num !== 'number'){
+        return 'Input must be a number!'
+    } else if (num < 0){
+        return 'Number must be positive'
     } else {
         return 'Try again!'
     }
-    }
+}
