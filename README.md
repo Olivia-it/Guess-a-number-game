0abb70df96c59db259af64f8e12471528d173823
BenFleck25 <ben.fleck2508@outlook.com>
2025-11-28 14:51:48 +0000

    started README and adding randomisation to script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Guess-a-number-game
+# Game 1 -> Guess a Number
+Our first game allows the user to input a number between 1 and 10 in the console. If the user guesses the same number as the computer is thinking, they win!
+
+
+
+# Game 2 -> Rock, Paper, Scissors
+Our second game is a classic game of rock, paper, scissors. Each game the computer will pick one of the three choices, rock, paper or scissors at random. The user will also input one of the three choices to see if they can beat the computer.
--- script.js
+++ script.js
@@ -11,12 +11,14 @@
 const prompt=require("prompt-sync")({sigint:true})
 let userInp = prompt("Enter a number between 1 and 10: ")
 let parsedInp = parseInt(userInp)
+let computerInp = Math.floor(Math.random() * 10);
+
 // function guessNumber(num){
-if (parsedInp === 7){
-        console.log('Correct Number!')
+if (parsedInp === computerInp){
+        console.log("You read the computer's mind!")
     } else if (parsedInp > 10){
         console.log('Number must be between 1 and 10')
-    } else if (typeof parsedInp == 'string'){
+    } else if (typeof use === 'string'){
         console.log('string detected!')
     } else if (parsedInp < 0){
         console.log('Number must be positive') 
