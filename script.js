cd03c64d7b9cb3440e16799b728d87c5eb8d0ecb
BenFleck25 <ben.fleck2508@outlook.com>
2025-11-28 14:55:20 +0000

    removed commented junk

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// const numForm = document.querySelector('#numForm')
-// const numResult = document.querySelector('#result')
-
-
-// numForm.addEventListener('submit', guessNumber(e))
-//     e.preventDefault();
-//     const userValue = Number(document.querySelector('#userNum').value)
-//     const message = guessNumber(userValue);
-//     numResult.textContent = message
-
 const prompt=require("prompt-sync")({sigint:true})
 let userInp = prompt("Enter a number between 1 and 10: ")
 let parsedInp = parseInt(userInp)
